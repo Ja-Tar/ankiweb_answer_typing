9e93ed0b74be26c29725a7164273d7bd254cbc33
Ja Tar <tarnogorskijan@gmail.com>
2024-02-25 16:18:38 +0100

    teraz lepiej :)

--- AnkiWeb-AnswerTyping.user.js
+++ AnkiWeb-AnswerTyping.user.js
@@ -2,10 +2,11 @@
 // @name        AnkiWeb - AnswerTyping
 // @namespace   JaTar Scripts
 // @match       https://ankiuser.net/study
-// @require     https://rawcdn.githack.com/google/diff-match-patch/62f2e689f498f9c92dbc588c58750addec9b1654/javascript/diff_match_patch.js
-// @require     https://rawcdn.githack.com/Ja-Tar/ankiweb_answer_typing/9852a2fb00376c62f0a195c36590d6b1af4cc082/compareTexts.js
+// @require     https://cdn.jsdelivr.net/npm/diff@5.2.0/dist/diff.min.js
+// @updateURL   https://raw.githubusercontent.com/JaTar/ankiweb_answer_typing/main/AnkiWeb-AnswerTyping.user.js
+// @downloadURL https://raw.githubusercontent.com/JaTar/ankiweb_answer_typing/main/AnkiWeb-AnswerTyping.user.js
 // @grant       none
-// @version     0.2.0
+// @version     1.0.0
 // @author      JaTar
 // ==/UserScript==
 
@@ -49,8 +50,10 @@ function sprawdzOdpowiedz(userAnswer) {
     }
 
     if (answerAttribute && hrElement) {
+        const diffs = diff(answerAttribute, userAnswer);
+        const diffOutput = renderDiffToHTML(diffs);
         const resultDiv = document.createElement('div');
-        resultDiv.innerHTML = compareTexts(answerAttribute, userAnswer);
+        resultDiv.innerHTML = diffOutput;
         try {
             resultDiv.id = 'result';
             qaDiv.insertBefore(resultDiv, document.querySelector('br'));
